083e6d0fb3bb311a70a94c0b9da4115f80b983f5
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2024-09-01 12:59:06 +0600

    Add homepage

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,10 +2,21 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Post extends Model
 {
     use HasFactory;
+
+    public function scopePublished($query)
+    {
+        $query->where('published_at', '<=', Carbon::now());
+    }
+
+    public function scopeFeatured($query)
+    {
+        $query->where('featured', true);
+    }
 }
